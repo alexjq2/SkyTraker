495bfd526f9c45b315e8241b0d14b7a12bd22fb1
alex <alexqcanchan@gmail.com>
2023-07-04 13:04:52 -0500

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,21 @@ function App() {
 
   return (
     <>
+    <nav>
+      <div>
+      <a href=""><img src="./src/assets/Logo.png" alt="logo" className='img-logo'/></a>
+      </div>
+      <div className='search-climate'>
+        <input type="text" placeholder="Busca una ciudad" className='search-climate_tracker' />
+        <button type="submit"><i className="fa-brands fa-searchengin"></i></button>
+      </div>
+      <div className='toggle-switch'>
+        <label>
+          <input type = 'checkbox' className='toggle-switch_input'/>
+          <span className = 'slider'></span>
+        </label>
+      </div>
+    </nav>
     </>
   )
 }
